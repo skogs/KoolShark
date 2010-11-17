40edbb7ef8cc66ac89d7419704a29789db5b31da
U-Mystery\skoganti <skoganti@Mystery.(none)>
2010-11-16 21:15:24 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KoolShark::Application.config.secret_token = 'bcf8dcbbf942b951cda09c404dbfd75bebf172ebf7379299190d31446fb5e14e8f3e53806b65e2f8320a5e48b5abe6757f9acd4b60fe8c2a6a013213b4d43128'
